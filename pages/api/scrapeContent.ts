384fdd41d5b38e4a364f03a57ea5be1fc2e6fdde
thm2320 <70251202+thm2320@users.noreply.github.com>
2021-03-05 14:27:25 +0800

    api for scraping posts from hackernews and store in memory

---
+++ pages/api/scrapeContent.ts
@@ -0,0 +1,41 @@
+import puppeteer from 'puppeteer';
+import cheerio from 'cheerio';
+import { posts } from 'src/data/posts';
+
+
+const scrapeContent = async () => {
+  const browser: puppeteer.Browser = await puppeteer.launch();
+  const page: puppeteer.Page = await browser.newPage();
+  await page.goto('https://news.ycombinator.com');
+
+  let content = await page.content();
+  const $ = cheerio.load(content);
+  $('.athing').each((idx, el) => {
+    const title = $(el).children('.title').children('.storylink').text();
+    const url = $(el).children('.title').children('.storylink').attr('href');
+
+    const subtextEl = $(el).next().children('.subtext');
+    const points = $(subtextEl).children('.score').text();
+    const author = $(subtextEl).children('.hnuser').text();
+    const time = $(subtextEl).children('.age').text();
+    const comments = $(subtextEl).children('a:contains("comment")').text();
+
+    posts.push({
+      title,
+      url,
+      points,
+      author,
+      time,
+      comments,
+    });
+  })
+
+  browser.close();
+  return posts;
+}
+
+
+export default async function handler(req, res) {
+  const posts = await scrapeContent();
+  res.status(200).json(posts);
+}
---
+++ src/data/posts.ts
@@ -0,0 +1,10 @@
+type Post = {
+  title: string,
+  url: string,
+  points: string,
+  author: string,
+  time: string,
+  comments: string,
+}
+
+export const posts: Post[] = [];
